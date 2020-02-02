ae925270827073ba89064744ea74b78a365e302a
lista <lista@bk.ru>
2020-02-02 23:52:11 +0200

    issue #25

--- src/main/java/com/lista/filmpoisk02/model/services/LookupId.java
+++ src/main/java/com/lista/filmpoisk02/model/services/LookupId.java
@@ -27,11 +27,62 @@ public class LookupId {
     public Page eval(String cSeekId, SpringBootConfiguration config,
                      SiteLookupService omDbApiLookupService) {
         String cUrl01;
+        Page oPage01 = getNewPage();
+
+        cUrl01 = get_cUrl01(cSeekId, config);
+
+        Future<Page> result = omDbApiLookupService.findPage(cUrl01, config.getApikey(), cSeekId);
+
+        try {
+            int timeOut = waitIsDone(result, config);
+            if (timeOut > 0) {
+                log.info("  // запись в экзепляр класса время чтения, с "
+                        + ((config.getTimeoutforsinglerequests() * 1000) - timeOut)/1000 );
+                oPage01 = result.get();
+            } else {
+                log.info("Время ожидания превышено Timeoutforsinglerequests, с" + config.getTimeoutforsinglerequests() );
+            }
+        } catch (InterruptedException e) {
+            log.error(e.getMessage() + " InterruptedException", e);
+        } catch (ExecutionException e) {
+            log.error(e.getMessage() + "ExecutionException", e);
+        }
+        checkStatusPage(cSeekId, oPage01);
+        return oPage01;
+    }
+
+    private Page getNewPage() {
         Page oPage01 = new Page(SUCCESS_STATUS, CODE_SUCCESS);
         oPage01.setStatus(ERROR_STATUS);
         oPage01.setCode(AUTH_FAILURE);
+        return oPage01;
+    }
 
+    private int waitIsDone(Future<Page> result, SpringBootConfiguration config) throws InterruptedException {
+        int timeOut = config.getTimeoutforsinglerequests() * 1000; // значение в секундах
+        while (!result.isDone()) {
+            Thread.sleep(PAUSEBETWEENEACHCHECK); //millisecond pause between each check
+            timeOut -= PAUSEBETWEENEACHCHECK;
+            if (timeOut <= 0) {
+                break;
+            }
+        }
+        return timeOut;
+    }
 
+    private void checkStatusPage(String cSeekId, Page oPage01) {
+        if (oPage01.getCode() == 102) {
+            cSeekId = "  Ошибвка поиска ID=" + cSeekId;
+            log.info(cSeekId + "  cPage:" + oPage01.getStatus());
+        } else {
+            log.info("oPage01:" + oPage01);
+        }
+
+        oPage01.setStatus(oPage01.getStatus() + " " + cSeekId);
+    }
+
+    private String get_cUrl01(String cSeekId, SpringBootConfiguration config) {
+        String cUrl01;
         if (Debug.FALSE) {
             log.info("--> " + "/filmpoisk-id");
             log.info("  config.getApikey()=" + config.getApikey());
@@ -44,42 +95,7 @@ public class LookupId {
         } else {
              cUrl01 = "";
         }
-
-        Future<Page> result = omDbApiLookupService.findPage(cUrl01, config.getApikey(), cSeekId);
-
-        try {
-            int timeOut = config.getTimeoutforsinglerequests() * 1000; // значение в секундах
-            while (!result.isDone()) {
-                Thread.sleep(PAUSEBETWEENEACHCHECK); //millisecond pause between each check
-                timeOut -= PAUSEBETWEENEACHCHECK;
-                if (timeOut <= 0) {
-                    break;
-                }
-            }
-            if (timeOut > 0) {
-                log.info("  // запись в экзепляр класса время чтения, с "
-                        + ((config.getTimeoutforsinglerequests() * 1000) - timeOut)/1000 );
-                oPage01 = result.get();
-            } else {
-                log.info("Время ожидания превышено Timeoutforsinglerequests, с" + config.getTimeoutforsinglerequests() );
-            }
-
-        } catch (InterruptedException e) {
-            log.error(e.getMessage() + " InterruptedException", e);
-        } catch (ExecutionException e) {
-            log.error(e.getMessage() + "ExecutionException", e);
-        }
-
-        if (oPage01.getCode() == 102) {
-            cSeekId = "  Ошибвка поиска ID=" + cSeekId;
-            log.info(cSeekId + "  cPage:" + oPage01.getStatus());
-        } else {
-            log.info("oPage01:" + oPage01);
-        }
-
-        oPage01.setStatus(oPage01.getStatus() + " " + cSeekId);
-
-        return oPage01;
+        return cUrl01;
     }
 
 }
