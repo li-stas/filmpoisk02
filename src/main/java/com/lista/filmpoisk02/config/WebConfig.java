45342b4b48edefbfbf751c144043806ba0edfa0c
lista <lista@bk.ru>
2021-01-13 19:12:57 +0200

    Избегайте неиспользуемых импортов

--- src/main/java/com/lista/filmpoisk02/config/WebConfig.java
+++ src/main/java/com/lista/filmpoisk02/config/WebConfig.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
